952211302b9e0f6172bab4a4ba156954617ede43
Kaiwei Wu <kaiwei-wu@hotmail.com>
2018-11-23 09:44:47 +1300

    first commit

---
+++ Api_01/Api_01/Data/Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api_01.Models
+{
+    public class Context : DbContext
+    {
+        public Context (DbContextOptions<Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Api_01.Models.Class> Class { get; set; }
+    }
+}
---
+++ Api_01/Api_01/Models/SeedData.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api_01.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new Context(
+                serviceProvider.GetRequiredService<DbContextOptions<Context>>()))
+            {
+                // Look for any movies.
+                if (context.Class.Count() > 0)
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Class.AddRange(
+                    new Class
+                    {
+                        Title = "Is Mayo an Instrument?",
+                        Url = "https://i.kym-cdn.com/photos/images/original/001/371/723/be6.jpg",
+                        Tags = "spongebob",
+                        Uploaded = "07-10-18 4:20T18:25:43.511Z",
+                        Width = "768",
+                        Height = "432"
+                    }
+
+
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
